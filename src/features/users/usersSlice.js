b65c612577e361b8e5a9d0a3fa95af5dd3a0bd88
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-06-22 12:54:44 +0100

    Update usersSlice.js

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,9 +1,9 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = [
-    {id: '0', name: 'Unnikrishnan MS'},
-    {id: '1', name: 'Vivek SS'},
-    {id: '2', name: 'Pranav Ajith'},
+    {id: '0', name: 'Morgan Wiley'},
+    {id: '1', name: 'Melon Potts'},
+    {id: '2', name: 'Mark Woods'},
 ];
 
 const usersSlice = createSlice({
