140a07e04509344f8c27255a27c0d722491a6015
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-06-23 02:46:34 +0100

    Added thunk middleware to perform async task
    Added Async thunk to fetch posts from API. Also provided extra reducers that handles the async fetch actions.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,29 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
-//Persists the posts state from the localStorage 
-const initialState = JSON.parse(localStorage.getItem('posts')) || [];
+import axios from 'axios';
+import { sub } from 'date-fns/esm';
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const initialState = {
+    posts:[],
+    status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get(POSTS_URL);
+    return response.data
+});
+
+
 const postSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
         addPosts: {
             reducer(state, action) {
-            state.push(action.payload);
+            state.posts.push(action.payload);
             },
             prepare(title, content, userId){
                 return{
@@ -31,7 +46,9 @@ const postSlice = createSlice({
         },
         deletePostById: {
             reducer(state, action) {
-                return state.filter(post => post.id !== action.payload.id);
+                return {posts: state.posts.filter(post => post.id !== action.payload.id),
+                status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+                error: null};
             },
 
             prepare(id){
@@ -43,16 +60,46 @@ const postSlice = createSlice({
             }
         },
         reactionAdded(state, action){
-            console.log(action.payload);
             const {postId, name} = action.payload;
-            console.log(postId, name);
-            const existingPost = state.find(post => post.id === postId);
+            const existingPost = state.posts.find(post => post.id === postId);
             if(existingPost){
                 existingPost.reactions[name]++;
             }
         }
+    },
+    extraReducers(builder){
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Adding date and reactions
+                console.log(action);
+                let min = 1;
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                });
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message;
+            })
     }
 });
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
+
 export default postSlice.reducer;
 export const {addPosts, deletePostById, reactionAdded} = postSlice.actions;
