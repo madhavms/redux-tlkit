1231e3f1edb0bf9ab042b96dab5971f67a882a7a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-06-23 02:47:45 +0100

    Added thunk middleware to get user data
    Added thunk to get user data from API. Also added the extra reducers to perform the actions.

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,17 +1,29 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [
-    {id: '0', name: 'Morgan Wiley'},
-    {id: '1', name: 'Melon Potts'},
-    {id: '2', name: 'Mark Woods'},
-];
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+        try{
+            const result = await axios.get(USERS_URL);
+            return result.data;
+        }
+        catch(err){
+            return err.message;
+        }    
+});
 
 const usersSlice = createSlice({
     name:'users',
     initialState,
-    reducers: {
-
-    }
+    reducers: {},
+    extraReducers(builder){
+        builder.addCase(fetchUsers.fulfilled, (state,action) => {
+                return action.payload;
+            })
+        }
 });
 
 export const selectAllUsers = (state) => state.users;
