519e9df7ccd1507d07ec5d1787055693a754260e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-06-23 03:28:05 +0100

    Added React strict mode

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <React.StrictMode>
   <Provider store={store}>
     <App />
   </Provider>
+  </React.StrictMode>
 
 );
 
