b46a15d6e3438c9dea8eca7577ea70d527c2cb56
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-06-23 02:51:22 +0100

    Removed strict mode and initiate user fetch

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
+import {fetchUsers} from './features/users/usersSlice'
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
   <Provider store={store}>
     <App />
   </Provider>
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
